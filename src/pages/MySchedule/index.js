90269396166e9c29cfc3af4fb9921da7d872c003
lucianovsjr <lucianovsjr@gmail.com>
2020-08-01 19:22:06 -0300

    Add my appointments

--- src/pages/MySchedule/index.js
+++ src/pages/MySchedule/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { parseISO, format } from 'date-fns';
+
+import api from '../../services/api';
 
 import Background from '../../components/Background';
 
+import { Container, MyHourList, ButtonCardHour, Title } from './styles';
+
 function MySchedule() {
-  return <Background />;
+  const [myHours, setMyHours] = useState([]);
+
+  useEffect(() => {
+    async function loadingMyHours() {
+      const response = await api.get('my-appointments');
+
+      if (response.status === 200) {
+        console.log(response.data);
+        setMyHours(
+          response.data.map((hour) => ({
+            id: hour.id,
+            time: format(parseISO(hour.date), 'dd/MM/yyyy HH:mm'),
+          }))
+        );
+      }
+    }
+
+    loadingMyHours();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <MyHourList
+          data={myHours}
+          keyExtractor={item => item.time}
+          renderItem={({item}) => (
+            <ButtonCardHour>
+              <Title>{item.date}{item.time}</Title>
+            </ButtonCardHour>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 export default MySchedule;
