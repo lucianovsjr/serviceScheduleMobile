bb2ee9caa1622e895d8245878a5105d795998852
lucianovsjr <lucianovsjr@gmail.com>
2020-12-05 17:54:32 -0300

    Fix BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://10.0.2.2:8000';
-//export const BASE_URL = 'https://lucianovsjr-calendar.herokuapp.com';
+const BASE_URL_DEV = 'http://10.0.2.2:8000';
+const BASE_URL_PROD = 'https://lucianovsjr-calendar.herokuapp.com';
+export const BASE_URL = __DEV__ ? BASE_URL_DEV : BASE_URL_PROD;
+// export const BASE_URL = 'https://lucianovsjr-calendar.herokuapp.com';
 
-const api = axios.create({
-  baseURL: `${BASE_URL}/api/`
-});
+const api = axios.create({ baseURL: `${BASE_URL}/api/` });
 
 export default api;
